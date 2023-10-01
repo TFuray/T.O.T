664d2a1524b79e2f6f35b8333119f4030e30025f
Thomas Furay <103867568+TFuray@users.noreply.github.com>
2023-10-01 02:19:29 -0700

    adding modal store

--- admin-dashboard/app/(root)/page.tsx
+++ admin-dashboard/app/(root)/page.tsx
@@ -1,10 +1,15 @@
+"use client"
+
 import { UserButton } from "@clerk/nextjs"
-import { User } from "lucide-react"
+import { Modal } from "@/components/ui/modal"
 
 export default function SetupPage() {
   return (
     <>
       <div className="p-4">
+        <Modal title="Test" description="Test Desc" isOpen onClose={() => { }}>
+          Children
+        </Modal>
         <UserButton afterSignOutUrl="/" />
         "This is a Protected Route"
       </div>
