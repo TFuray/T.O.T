fe31b9d27915c7872e6d9b254a0ee47aa4a3be1a
Thomas Furay <103867568+TFuray@users.noreply.github.com>
2023-10-01 15:56:32 -0700

    added naming form to modal

--- admin-dashboard/app/(root)/page.tsx
+++ admin-dashboard/app/(root)/page.tsx
@@ -1,18 +1,24 @@
 "use client"
 
+import { useEffect } from "react"
 import { UserButton } from "@clerk/nextjs"
+
 import { Modal } from "@/components/ui/modal"
+import { useStoreModal } from "@/hooks/use-store-modal"
 
 export default function SetupPage() {
+  const onOpen = useStoreModal((state) => state.onOpen)
+  const isOpen= useStoreModal((state) => state.isOpen)
+
+  useEffect(() => {
+    if (!isOpen) {
+    onOpen()
+  }
+  }, [isOpen, onOpen])
+
   return (
-    <>
-      <div className="p-4">
-        <Modal title="Test" description="Test Desc" isOpen onClose={() => { }}>
-          Children
-        </Modal>
-        <UserButton afterSignOutUrl="/" />
-        "This is a Protected Route"
-      </div>
-    </>
+    <div className="p-4">
+      Root Page
+    </div>
   )
 }
