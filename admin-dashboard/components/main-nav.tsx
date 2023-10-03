51c068934de83bd7985bb01a75455ae2bda78643
Thomas Furay <103867568+TFuray@users.noreply.github.com>
2023-10-02 23:53:54 -0700

    designing navbar

--- admin-dashboard/components/main-nav.tsx
+++ admin-dashboard/components/main-nav.tsx
@@ -1,8 +1,9 @@
 "use client"
 
-import { cn } from "@/lib/utils"
 import Link from "next/link"
 import { useParams, usePathname } from "next/navigation"
+
+import { cn } from "@/lib/utils"
 
 export function MainNav({
   className,
@@ -14,17 +15,27 @@ export function MainNav({
   const routes = [
     {
       href: `/${params.storeId}/settings`,
-      label: 'settings',
-      active: pathname === `/${params.storeId}/settings`
-    }
+      label: "settings",
+      active: pathname === `/${params.storeId}/settings`,
+    },
   ]
 
   return (
-    <nav className={cn("flex items-center space-x-4 lg:space-x-6", className )}>
-      {routes.map((route) => {
-        <Link key={route.href}>
+    <nav className={cn("flex items-center space-x-4 lg:space-x-6", className)}>
+      {routes.map((route) => (
+        <Link
+          key={route.href}
+          href={route.href}
+          className={cn(
+            "text-sm font-medium transition-colors hover:text-primary",
+            route.active
+              ? "text-black dark:text-white"
+              : "text-muted-foreground"
+          )}
+        >
+          {route.label}
         </Link>
-    })}
-  </nav>
+      ))}
+    </nav>
   )
 }
