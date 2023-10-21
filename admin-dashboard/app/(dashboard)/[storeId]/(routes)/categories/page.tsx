cbd1bd76597731451b8535c66fb7dd31b38427e2
Thomas Furay <103867568+TFuray@users.noreply.github.com>
2023-10-21 12:54:49 -0700

    ading components section

--- admin-dashboard/app/(dashboard)/[storeId]/(routes)/categories/page.tsx
+++ admin-dashboard/app/(dashboard)/[storeId]/(routes)/categories/page.tsx
@@ -1,30 +1,34 @@
 import {format } from 'date-fns'
 import prismadb from "@/lib/prismadb"
 import { BillboardClient } from "./components/client"
-import { BillboardColumn } from "./components/columns"
+import { CategoryColumn } from "./components/columns"
 
-const BillboardsPage = async ({ params }: { params: { storeId: string } }) => {
-  const billboards = await prismadb.billboard.findMany({
+const CategoriesPage = async ({ params }: { params: { storeId: string } }) => {
+  const categories = await prismadb.category.findMany({
     where: {
       storeId: params.storeId,
+    },
+    include: {
+      billboard: true,
     },
     orderBy: {
       createdAt: "desc",
     },
   })
 
-  const formattedBillbolards: BillboardColumn[] = billboards.map((item) => ({
+  const formattedCategories: CategoryColumn[] = categories.map((item) => ({
     id: item.id,
-    label: item.label,
+    name: item.name,
+    billboardLabel: item.billboard.label,
     createdAt: format(item.createdAt, "MMM do, yyyy")
   }) )
 
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8  pt-6">
-        <BillboardClient data={formattedBillbolards} />
+        <BillboardClient data={formattedCategories} />
       </div>
     </div>
   )
 }
-export default BillboardsPage
+export default CategoriesPage
