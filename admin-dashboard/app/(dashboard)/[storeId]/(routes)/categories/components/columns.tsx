242e9419c5d2160a0fad0ca8e338d7d748158685
Thomas Furay <103867568+TFuray@users.noreply.github.com>
2023-10-25 21:50:43 -0700

    ading components section

--- admin-dashboard/app/(dashboard)/[storeId]/(routes)/categories/components/columns.tsx
+++ admin-dashboard/app/(dashboard)/[storeId]/(routes)/categories/components/columns.tsx
@@ -24,6 +24,7 @@ export const columns: ColumnDef<CategoryColumn>[] = [
     accessorKey: "createdAt",
     header: "Date",
   },
+  
   {
     id: "actions",
     cell: ({ row }) => <CellAction data={row.original} />,
